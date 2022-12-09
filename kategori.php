c854f177a583a4906c1849496947d8e43e9fbe52
hansenjonatann <hansenjonatann@gmail.com>
2022-12-09 08:01:02 +0700

    merubah judul pada setiap halaman

--- kategori.php
+++ kategori.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'config.php';
+require 'kirim.php';
 
 $result = mysqli_query($conn, "SELECT * FROM kirimpuisi");
 
@@ -13,7 +13,7 @@ $result = mysqli_query($conn, "SELECT * FROM kirimpuisi");
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Kategori Taman Puisi</title>
+  <title>Taman Puisi | Kategori</title>
   <link rel="stylesheet" href="kategori.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
